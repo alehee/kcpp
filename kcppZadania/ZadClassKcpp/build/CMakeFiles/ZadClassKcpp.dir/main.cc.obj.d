33f7b51f518e2b47471a6d1c7e1a0b94c9ef53d2
alehee <aleksanderheese@o2.pl>
2023-06-24 22:09:56 +0200

    Zadanie 7 hotfix

---
+++ kcppZadania/ZadClassKcpp/build/CMakeFiles/ZadClassKcpp.dir/main.cc.obj.d
@@ -0,0 +1,5 @@
+CMakeFiles/ZadClassKcpp.dir/main.cc.obj: \
+ D:\Proj_Programming\CPP\kcpp\kcppZadania\ZadClassKcpp\main.cc \
+ D:/Proj_Programming/CPP/kcpp/kcppZadania/ZadClassKcpp/include/ZadKcpp.h \
+ D:/Proj_Programming/CPP/kcpp/kcppZadania/ZadClassKcpp/include/Funkcje.h \
+ D:/Proj_Programming/CPP/kcpp/kcppZadania/ZadClassKcpp/include/WeWy.h
